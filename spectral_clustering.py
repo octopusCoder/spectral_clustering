12385c44293e974b1e813020f7c78bc1eb8ba79b
octopusCoder <15626107216@163.com>
2016-12-26 16:25:49 +0800

    presort2000 analyze

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -10,44 +10,30 @@ from scipy.sparse.linalg.eigen import arpack
 import time
 
 def calculate_similarities():
-    courses = [line.strip() for line in open('new2000')]
-    #courses_name = [course.split('\t')[0] for course in courses]
-    #print(courses_name[0:10])
+    items = [line.strip() for line in open('new2000')]
 
-    #texts_lower = [[word for word in document.lower().split()]for document in courses]
-    #print(texts_lower[0])
     print("begin to calculate similarity")
 
     '''
-    texts_tokenized = [[word.lower() for word in word_tokenize(document)] for document in courses]
-    print(texts_tokenized[0])
+    texts_tokenized = [[word.lower() for word in word_tokenize(document)] for document in items]
 
     english_stopwords = stopwords.words('english')
-    #print(english_stopwords)
     print("filter stopwords")
     texts_filtered_stopwords = [[word for word in document if not word in english_stopwords] for document in texts_tokenized]
-    print(texts_filtered_stopwords[0])
 
     print("remove punctuations")
     english_punctuations = [',','.',':',';','(',')','[',']','{','}','&','!','@','#','%','$','*']
     texts_filtered = [[word for word in document if not word in english_punctuations]for document in texts_filtered_stopwords]
-    print(texts_filtered[0])
 
+
+    #stemmed words
     print("stemmed")
     st = LancasterStemmer()
     texts_stemmed = [[st.stem(word) for word in document]for document in texts_filtered]
-    print(texts_stemmed[0])
-
-    all_stems = sum(texts_stemmed, [])
-    stems_once = set(stem for stem in set(all_stems) if all_stems.count(stem) == 1)
-    texts = [[stem for stem in text if stem not in stems_once]for text in texts_stemmed]
-
-    texts = texts_stemmed
-
     '''
 
-    texts = [[word for word in document] for document in courses]
-
+    #new2000 has been processed
+    texts = [[word for word in document] for document in items]
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
     dictionary = corpora.Dictionary(texts)
@@ -88,19 +74,13 @@ def spectral_clustering(similarity, sigma, clusters):
 
     sqrt_sum = sqrt(multiply(vectors,vectors).sum(1))
     row, column = shape(vectors)
-    #print(row, column)
+
     for i in range(row):
         for j in range(column):
             vectors[i][j] = vectors[i][j] / sqrt_sum[i]
-            #print(vectors[i][j])
-        #print(" ")
     print("perform k-means")
     centers, clusters_category = kMeans(vectors, clusters)
 
-    '''
-    for i in range(shape(clusters_category)[0]):
-        print(clusters_category[i,0])
-    '''
     return clusters_category
 
 def random_centers(data, clusters):
@@ -108,9 +88,7 @@ def random_centers(data, clusters):
     centers = mat(zeros((clusters,k)))
     for j in range(k):
         minum = min(data[:,j])
-        #print(minum)
         distance = max(data[:,j]) - minum
-        #print(distance)
         centers[:,j] = mat(minum.real + distance.real * random.rand(k,1))
     return centers
 
@@ -143,39 +121,17 @@ def kMeans(data, clusters):
     return centers, clusters_category
 
 if __name__ == '__main__':
-    '''
+    
     similarity = calculate_similarities()
     clusters_category = spectral_clustering(similarity,20,10)
-    #clusters_category.dtype = 'int32'
     n = shape(clusters_category)[0]
     result = [0]*10
     clusters_category_int = clusters_category.astype(int)
     for i in range(n):
         print(clusters_category_int[i,0])
         result[clusters_category_int[i, 0]]+=1
-    '''
-    result = [172, 292, 269, 265, 87, 113, 257, 144, 201, 81]
     result.sort()
     print(result)
-
-    '''
-    n = shape(clusters_category)[0]
-    name_cluster = [("none", 0)] * n
-    for i in range(n):
-        tup = (clusters_category[i,0], courses_name[i])
-        name_cluster[i] = tup
-        #print(tup)
-    name_cluster.sort(key=lambda x:x[0])
-    #print(name_cluster)
-    print("write result to file")
-    f = open("result", "w")
-    for i in range(n):
-        tup = name_cluster[i]
-        cluster = tup[0]; name = tup[1]
-        item = str(cluster) + " " + str(name) + "\n"
-        f.write(item)
-    f.close()
-    '''
     print("complete")
 
 
